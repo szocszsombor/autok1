5c853a9060a86e4fe4d3df3cf5a7aa267a6e3855
autok <szs.petrik2@gmail.com>
2020-02-17 09:28:23 +0100

    csok

--- autok/Program.cs
+++ autok/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace autok
 {
@@ -10,7 +11,11 @@ namespace autok
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("anyad");
+
+
+            StreamReader sr = new StreamReader("auto.csv");
+
+
         }
     }
 }
---
+++ autok/autocskak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok
+{
+    class autocskak
+    {
+        private string _marka;
+        private string _modell;
+        private int _evjarat;
+        private int _kilometer;
+        private string _uzemanyag;
+        private int _ára;
+
+
+
+    }
+}
